1c1a874c80c16189a2a52266949286694671810c
Frederik Creemers <frederikcreemers@gmail.com>
2017-12-07 05:38:05 +0100

    Day 5

--- day4_1.py
+++ day4_1.py
@@ -1,7 +1,7 @@
 def count_valid_passphrazes(f):
     #file is the system passphraze file.
     is_valid = lambda p: len(p.strip().split()) == len(set(p.strip().split()))
-    return len(filter(is_valid, f))
+    return len(filter(is_valid, file))
     
 
 if __name__ == "__main__":
---
+++ day5_2.py
@@ -0,0 +1,19 @@
+def count_steps(f):
+    instrs = [int(l) for l in f]
+    
+    pos = 0
+    steps = 0
+    
+    while 0 <= pos < len(instrs):
+        ins = instrs[pos]
+        if ins < 3:
+            instrs[pos] += 1
+        else:
+            instrs[pos] -= 1
+        pos += ins
+        steps += 1
+    
+    return steps
+
+if __name__ == "__main__":
+    print count_steps(open("day5_input.txt"))
